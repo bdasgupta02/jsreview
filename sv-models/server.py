5e5ec05fe3f1d691bd59a6969f716e6353052495
bdasgupta02 <bdasgupta02@gmail.com>
2023-02-28 23:49:14 +0800

    finish testing and dev of vuln endpoint

--- sv-models/server.py
+++ sv-models/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from models.vulnerability import *
+import json
 
 app = Flask(__name__)
 
@@ -12,25 +13,26 @@ def welcome():
 
 
 @app.route('/predict/vulnerability', methods=['POST'])
-def predict_vuln():
+def ep_predict_vuln():
     try:
         data = request.json
         pred = predict_vuln(
             model_vuln,
-            mccc=data['mccc'],
-            loc=data['loc'],
-            tlloc=data['tlloc'],
-            tloc=data['tloc'],
-            hor_d=data['hor_d'],
-            hon_d=data['hon_d'],
-            hon_t=data['hon_t'],
-            hvoc=data['hvoc'],
-            hdiff=data['hdiff'],
-            cycl=data['cycl'],
+            data['mccc'],
+            data['loc'],
+            data['tlloc'],
+            data['tloc'],
+            data['hor_d'],
+            data['hon_d'],
+            data['hon_t'],
+            data['hvoc'],
+            data['hdiff'],
+            data['cycl'],
         )
-        return {"pred": pred}
-    except:
-        return {"error": "cannot predict"}
+        pred = pred.item()
+        return json.dumps({"pred": pred})
+    except Exception as e:
+        return {"error": e}
 
 
 @app.route('/predict/bugs', methods=['POST'])
