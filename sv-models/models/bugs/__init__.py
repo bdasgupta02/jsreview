bc9addcdc41a1c7791ba8836a0ed5513e4f088a8
bdasgupta02 <bdasgupta02@gmail.com>
2023-03-01 18:29:06 +0800

    add multi func support for vuln endpoint

---
+++ sv-models/models/bugs/__init__.py
@@ -0,0 +1,2 @@
+def predict_bugs_func(func):
+    return []
--- sv-models/server.py
+++ sv-models/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_cors import CORS
 from models.vulnerability import *
+from models.bugs import *
 import json
 
 app = Flask(__name__)
@@ -10,37 +11,45 @@ model_vuln = get_model_vuln()
 
 
 @app.route('/ping', methods=['GET'])
-def welcome():
+def ping():
     return "pong"
 
 
-@app.route('/predict/vulnerability', methods=['POST'])
+@app.route('/predict/multiple/vuln', methods=['POST'])
 def ep_predict_vuln():
     try:
         data = request.json
-        pred = predict_vuln(
-            model_vuln,
-            mccc=data['mccc'],
-            loc=data['loc'],
-            tlloc=data['tlloc'],
-            tloc=data['tloc'],
-            hor_d=data['hor_d'],
-            hon_d=data['hon_d'],
-            hon_t=data['hon_t'],
-            hvoc=data['hvoc'],
-            hdiff=data['hdiff'],
-            cycl=data['cycl'],
-        )
-        pred = pred.item()
-        return json.dumps({"pred": pred})
+        resp = []
+        for func in data:
+            pred = predict_vuln(
+                model_vuln,
+                mccc=func['mccc'],
+                loc=func['loc'],
+                tlloc=func['tlloc'],
+                tloc=func['tloc'],
+                hor_d=func['hor_d'],
+                hon_d=func['hon_d'],
+                hon_t=func['hon_t'],
+                hvoc=func['hvoc'],
+                hdiff=func['hdiff'],
+                cycl=func['cycl'],
+            )
+            resp.append(pred.item())
+        return json.dumps(resp)
     except Exception as e:
         return {"error": e}
 
 
-@app.route('/predict/bugs', methods=['POST'])
-def predict_bugs():
-    # data should be array of functions
-    return "todo"
+@app.route('/predict/multiple/bugs', methods=['POST'])
+def ep_predict_bugs():
+    try:
+        data = request.json
+        resp = []
+        for func in data:
+            resp.append(predict_bugs_func(func))
+        return resp
+    except Exception as e:
+        return {"error": e}
 
 
 if __name__ == '__main__':
